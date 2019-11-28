842ff824a573c0432ded3f90d13b423a90293006
Minikitteh <43260087+Minikitteh@users.noreply.github.com>
2019-11-27 22:32:04 -0700

    Update P3.java

--- P3.java
+++ P3.java
@@ -7,17 +7,15 @@ public class P3 {
 
 	 public int numberOfArithmeticSlices(int[] A) {
 	        int count = 0;
-	        for (int i = 0; i < A.length - 2; i++) {
-	            int d = A[i + 1] - A[i];
-	            for (int j = i + 2; j < A.length; j++) {
-	                int k = 0;
-	            	for (k = i + 1; k <= j; k++)
-	                    if (A[k] - A[k - 1] != d)
-	                        break;
-	                if (k > j)
-	                    count++;
-	            }
-	        }
-	        return count;
-	    }
+		for (int i = 0; i < A.length - 2; i++) {
+			int d = A[i + 1] - A[i];
+			for (int j = i + 2; j < A.length; j++) {
+				if (A[j] - A[j - 1] == d)
+					count++;
+				else
+					break;
+			}
+		}
+		return count;
+	}
 }
